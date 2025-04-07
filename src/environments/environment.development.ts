cc6102c76b2bef2e8b94dc914685142ffac3e8d4
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2025-04-06 20:38:24 -0500

    feat: Add environment configurations for development and production

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  companyName: 'Gifs',
+  companySubName: 'App',
+  companySlogan: 'App para gestionar tus gifs',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  companyName: 'Gifs',
+  companySubName: 'App',
+  companySlogan: 'App para gestionar tus gifs',
+};
