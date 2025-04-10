8b69ec40d45d5af087ff973283b398265f4ff26d
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2025-04-09 23:14:59 -0500

    feat: Recreate environment configuration files and remove obsolete scripts

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  companyName: 'Gifs',
+  companySubName: 'App',
+  companySlogan: 'App para gestionar tus gifs',
+
+  // API key
+  giphyApiKey: 'MaatMhNMGTGu2Cp8E9Jd1SZw4wBDCgFP',
+  giphyUrl: 'https://api.giphy.com/v1',
+};
